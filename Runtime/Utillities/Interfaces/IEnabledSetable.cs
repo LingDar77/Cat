22b1c44f0665cd127b637b9b5e7c33ec158b663d
lingdar77 <lingdar77@outlook.com>
2023-12-12 16:37:38 +0800

    1

--- Runtime/Utillities/Interfaces/IEnabledSetable.cs
+++ Runtime/Utillities/Interfaces/IEnabledSetable.cs
@@ -2,7 +2,9 @@ namespace SFC
 {
     public interface IEnabledSetable
     {
+#pragma warning disable IDE1006 // 命名样式
         bool enabled { get; set; }
+#pragma warning restore IDE1006 // 命名样式
     }
 
 }
--- Runtime/Utillities/Interfaces/ITransformGetable.cs
+++ Runtime/Utillities/Interfaces/ITransformGetable.cs
@@ -4,6 +4,8 @@ namespace SFC
 {
     public interface ITransformGetable
     {
+#pragma warning disable IDE1006 // 命名样式
         Transform transform { get; }
+#pragma warning restore IDE1006 // 命名样式
     }
 }
