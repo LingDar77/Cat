4a52fad84ee58186c0a0eaf31684979436aab77e
lingdar77 <lingdar77@outlook.com>
2024-01-10 15:36:24 +0800

    1

--- Runtime/EventDispatchSystem/BuiltinEventDispatcher.cs
+++ Runtime/EventDispatchSystem/BuiltinEventDispatcher.cs
@@ -3,34 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace TUI.EventDispatchSystem
 {
-    public enum GameEvent
+    public class BuiltinEventDispatcher : MonoBehaviour, IEventDispatchSystem<string>
     {
-        Event1,
-        Event2
-    }
-    public class BuiltinEventDispatcher : MonoBehaviour, IEventDispatchSystem<GameEvent>
-    {
-        protected Queue<GameEvent> dispatchQueue = new();
+#if UNITY_EDITOR
+        [SerializeField] float MaxDispatchTime = 1f;
+#endif
+        protected Queue<string> dispatchQueue = new();
         protected Queue<EventParam> dispatchParamQueue = new();
 
         protected bool isDispatching = false;
-        protected readonly Dictionary<GameEvent, HashSet<System.Action<EventParam>>> events = new();
+        protected readonly Dictionary<string, HashSet<System.Action<EventParam>>> events = new();
 
         protected virtual void OnEnable()
         {
-            if (IEventDispatchSystem<GameEvent>.Singleton != null) return;
+            if (IEventDispatchSystem<string>.Singleton != null) return;
 
-            IEventDispatchSystem<GameEvent>.Singleton = this;
+            IEventDispatchSystem<string>.Singleton = this;
             DontDestroyOnLoad(transform.root.gameObject);
         }
 
         protected virtual void OnDisable()
         {
-            if (IEventDispatchSystem<GameEvent>.Singleton.transform != transform) return;
-            IEventDispatchSystem<GameEvent>.Singleton = null;
+            if (IEventDispatchSystem<string>.Singleton.transform != transform) return;
+            IEventDispatchSystem<string>.Singleton = null;
         }
 
-        public virtual void Dispatch(GameEvent type, EventParam data)
+        public virtual void Dispatch(string type, EventParam data)
         {
             dispatchQueue.Enqueue(type);
             dispatchParamQueue.Enqueue(data);
@@ -40,11 +38,15 @@ namespace TUI.EventDispatchSystem
         private IEnumerator DispatchAllEvents()
         {
             isDispatching = true;
+
+#if UNITY_EDITOR
+            var time = Time.realtimeSinceStartup;
+#endif
             while (dispatchQueue.Count != 0)
             {
                 var type = dispatchQueue.Dequeue();
                 var data = dispatchParamQueue.Dequeue();
-                
+
                 if (events.TryGetValue(type, out var actions))
                 {
                     foreach (var action in actions)
@@ -53,11 +55,20 @@ namespace TUI.EventDispatchSystem
                         yield return CoroutineHelper.nextUpdate;
                     }
                 }
+
+#if UNITY_EDITOR
+                var currentTime = Time.realtimeSinceStartup;
+                if (currentTime - time > MaxDispatchTime)
+                {
+                    Debug.LogWarning($"One event dispatching process is taking too much time, may event loops are triggered. Last excuted event type: {type}. Forcing the current dispatching process to stop.");
+                    break;
+                }
+#endif
             }
             isDispatching = false;
         }
 
-        public virtual void Subscribe(GameEvent type, System.Action<EventParam> callback)
+        public virtual void Subscribe(string type, System.Action<EventParam> callback)
         {
             if (events.TryGetValue(type, out var list))
             {
@@ -68,7 +79,7 @@ namespace TUI.EventDispatchSystem
             events.Add(type, new() { callback });
         }
 
-        public virtual void Unsubscribe(GameEvent type, System.Action<EventParam> callback)
+        public virtual void Unsubscribe(string type, System.Action<EventParam> callback)
         {
             if (events.TryGetValue(type, out var list))
             {
