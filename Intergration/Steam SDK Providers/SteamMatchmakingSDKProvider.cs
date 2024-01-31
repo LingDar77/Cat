6f387f9955b87673ba943788443f9429ebf2c987
LingDar77 <lingdar77@outlook.com>
2024-01-31 17:40:29 +0800

    1

--- Intergration/Steam SDK Providers/SteamMatchmakingSDKProvider.cs
+++ Intergration/Steam SDK Providers/SteamMatchmakingSDKProvider.cs
@@ -1,3 +1,7 @@
+#if !(UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX || STEAMWORKS_WIN || STEAMWORKS_LIN_OSX)
+#define DISABLE_STEAMWORKS
+#endif
+#if !DISABLE_STEAMWORKS
 using System;
 using TUI.SDKProvider;
 using TUI.Utillities;
@@ -82,3 +86,4 @@ namespace TUI.Intergration.SteamSDKProviders
         }
     }
 }
+#endif
