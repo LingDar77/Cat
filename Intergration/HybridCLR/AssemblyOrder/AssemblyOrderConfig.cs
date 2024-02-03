8b30dc4630b388804e34b1b4c1dc932f946846bf
LingDar77 <lingdar77@outlook.com>
2024-02-04 03:37:02 +0800

    1

---
+++ Intergration/HybridCLR/AssemblyOrder/AssemblyOrderConfig.cs
@@ -0,0 +1,11 @@
+#if UNITY_EDITOR
+
+namespace TUI.Intergration.HybridCLR
+{
+    using TUI.EditorScript;
+    public class AssemblyOrderConfig : ConfigableObject<AssemblyOrderConfig>
+    {
+
+    }
+}
+#endif
