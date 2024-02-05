7a6d8f41e27ca7f3bd5e697c7879794a432cd0cf
LingDar77 <lingdar77@outlook.com>
2024-02-05 19:05:04 +0800

    1

--- Runtime/ObjectPool/Interfaces/IObjectPool.cs
+++
@@ -1,9 +0,0 @@
-namespace TUI.ObjectPool
-{
-    public interface IObjectPool<Type> where Type : IPooledObject<Type>, new()
-    {
-        int Count { get; }
-        Type Get();
-        void Return(Type obj);
-    }
-}
--- Runtime/ObjectPool/Interfaces/IPooledObject.cs
+++
@@ -1,8 +0,0 @@
-namespace TUI.ObjectPool
-{
-    public interface IPooledObject<Type> : System.IDisposable where Type : IPooledObject<Type>, new()
-    {
-        IObjectPool<Type> Pool { get; set; }
-        
-    }
-}
