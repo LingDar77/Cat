4cc3587dbcf6ad2fb749e7089a4ca8937cfb56ca
LingDar77 <lingdar77@outlook.com>
2024-02-08 01:02:14 +0800

    1

---
+++ Runtime/ModifierSystem/ModifierManager.cs
@@ -0,0 +1,12 @@
+namespace Cat.ModifierSystem
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+    public class ModifierManager : MonoBehaviour
+    {
+        public List<string> Modifiers;
+
+
+
+    }
+}
