a99a0bb0024aa5f526c0dd4869bf54098c014237
LingDar77 <lingdar77@outlook.com>
2024-02-29 22:27:24 +0800

    1

---
+++ Intergration/HybridCLR/HotUpdateLoader/AssemblyOrder.cs
@@ -0,0 +1,10 @@
+namespace Cat.Intergration.Hybridclr
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class AssemblyOrder : ScriptableObject
+    {
+        public List<string> Assemblies = new();
+    }
+}
--- Intergration/HybridCLR/HotUpdateLoader/HotUpdateLoaderConfig.cs
+++ Intergration/HybridCLR/HotUpdateLoader/HotUpdateLoaderConfig.cs
@@ -3,13 +3,16 @@ namespace Cat.Intergration.Hybridclr
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
     using Cat.Utillities;
-    using HybridCLR.Editor.Settings;
+    using Mono.Cecil;
     using UnityEditor;
+    using UnityEditor.AddressableAssets;
     using UnityEditor.AddressableAssets.Settings;
-    using UnityEditor.Build.Player;
+    using UnityEditorInternal;
     using UnityEngine;
-    using UnityEngine.AddressableAssets;
 
     public class HotUpdateLoaderConfig : ConfigableObject<HotUpdateLoaderConfig>
     {
@@ -20,8 +23,7 @@ namespace Cat.Intergration.Hybridclr
 
     public class HotUpdateLoaderConfigWindow : ConfigWindow<HotUpdateLoaderConfig>
     {
-        // private Vector2 scrollPosition;
-
+        private static AssemblyOrder orderAsset;
 
         [MenuItem("Window/Cat/HotUpdate Loader")]
         private static void ShowWindow()
@@ -43,10 +45,191 @@ namespace Cat.Intergration.Hybridclr
             UnityEditor.Compilation.CompilationPipeline.compilationFinished += OnCompilationFinished;
         }
 
-        private static void OnCompilationFinished(object obj)
+        private static void OnCompilationFinished(object _)
         {
-            Debug.Log("compliation finished");
+            var config = HotUpdateLoaderConfig.Get();
+            var aa = AddressableAssetSettingsDefaultObject.GetSettings(false);
+            if (aa == null || !config.Enabled || config.AssemblyGroup == null) return;
+
+            var tempFolder = HybridCLR.Editor.Settings.HybridCLRSettings.Instance.outputAOTGenericReferenceFile;
+            tempFolder = $"Assets/{Path.GetDirectoryName(tempFolder)}";
+
+
+            ConfigGroup(config, aa, tempFolder);
+
+            CopyCompiledDlls(config, aa, tempFolder);
+
+            UpdateAssemblyOrder();
+
         }
+
+        private static void UpdateAssemblyOrder()
+        {
+            //Update AssemblyOrder
+            orderAsset.Assemblies.Clear();
+
+            orderAsset.Assemblies.AddRange(HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateAssemblies);
+
+            var assemblies = HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateAssemblyDefinitions;
+
+            orderAsset.Assemblies.AddRange(GetSortedAssemblies(assemblies));
+
+            if (orderAsset.Assemblies.Find(e => e == "Assembly-CSharp") != null)
+            {
+                orderAsset.Assemblies.Remove("Assembly-CSharp");
+                orderAsset.Assemblies.Add("Assembly-CSharp");
+            }
+
+            EditorUtility.SetDirty(orderAsset);
+        }
+
+        private static void CopyCompiledDlls(HotUpdateLoaderConfig config, AddressableAssetSettings aa, string tempFolder)
+        {
+            //Copy compiled dlls
+            var dllFolder = HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateDllCompileOutputRootDir;
+            dllFolder = $"{Environment.CurrentDirectory}/{dllFolder}";
+            var tempDllFolder = $"{Environment.CurrentDirectory}/{tempFolder}/Assemblies";
+
+            if (!Directory.Exists(tempDllFolder))
+            {
+                Directory.CreateDirectory(tempDllFolder);
+            }
+
+            foreach (var dll in HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateAssemblies)
+            {
+                var dllpath = $"{dllFolder}/{EditorUserBuildSettings.activeBuildTarget}/{dll}.dll";
+                if (File.Exists(dllpath))
+                {
+                    File.Copy(dllpath, $"{tempDllFolder}/{dll}.{EditorUserBuildSettings.activeBuildTarget}.bytes", true);
+                }
+            }
+
+            foreach (var dll in HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateAssemblyDefinitions)
+            {
+                var dllpath = $"{dllFolder}/{EditorUserBuildSettings.activeBuildTarget}/{dll.name}.dll";
+                if (File.Exists(dllpath))
+                {
+                    File.Copy(dllpath, $"{tempDllFolder}/{dll.name}.{EditorUserBuildSettings.activeBuildTarget}.bytes", true);
+                }
+            }
+
+            AssetDatabase.Refresh();
+
+            //Add to aa
+
+            foreach (var dll in HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateAssemblies)
+            {
+                var guid = AssetDatabase.AssetPathToGUID($"{tempFolder}/Assemblies/{dll}.{EditorUserBuildSettings.activeBuildTarget}.bytes");
+                var dllEntry = aa.CreateOrMoveEntry(guid, config.AssemblyGroup, true);
+                dllEntry.SetAddress($"{dll}.{EditorUserBuildSettings.activeBuildTarget}.bytes");
+            }
+
+            foreach (var dll in HybridCLR.Editor.Settings.HybridCLRSettings.Instance.hotUpdateAssemblyDefinitions)
+            {
+                var guid = AssetDatabase.AssetPathToGUID($"{tempFolder}/Assemblies/{dll.name}.{EditorUserBuildSettings.activeBuildTarget}.bytes");
+                var dllEntry = aa.CreateOrMoveEntry(guid, config.AssemblyGroup, true);
+                dllEntry.SetAddress($"{dll.name}.{EditorUserBuildSettings.activeBuildTarget}.bytes");
+            }
+        }
+
+        private static string[] GetSortedAssemblies(AssemblyDefinitionAsset[] assemblies)
+        {
+            var map = new Dictionary<string, List<string>>();
+            var roots = new List<string>();
+            var sorted = new List<string>();
+            foreach (var definitionAsset in assemblies)
+            {
+                var guid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(definitionAsset));
+                var matches = Regex.Matches(definitionAsset.ToString(), "\"GUID:(.+)\"");
+                var references = new List<string>();
+                for (int i = 0; i != matches.Count; ++i)
+                {
+                    references.Add(matches[i].Groups[1].Value);
+                }
+                map.Add(guid, references);
+            }
+
+
+            //find roots
+            foreach (var guid in map.Keys)
+            {
+                bool isRoot = true;
+                foreach (var item in map)
+                {
+                    if (item.Value.Contains(guid))
+                    {
+                        isRoot = false;
+                        break;
+                    }
+                }
+                if (isRoot)
+                {
+                    roots.Add(guid);
+                }
+            }
+
+            foreach (var root in roots)
+            {
+                List<string> result = new();
+                Queue<string> queue = new();
+                queue.Enqueue(root);
+                while (queue.Count != 0)
+                {
+                    var current = queue.Dequeue();
+                    if (!map.ContainsKey(current)) continue;
+                    result.Add(current);
+                    foreach (var child in map[current])
+                    {
+                        if (!map.ContainsKey(child)) continue;
+                        queue.Enqueue(child);
+                    }
+                }
+                result.Reverse();
+                sorted.AddRange(result);
+            }
+
+            var results = new List<string>();
+            foreach (var sortedItem in sorted)
+            {
+                var asset = AssetDatabase.LoadAssetAtPath<AssemblyDefinitionAsset>(AssetDatabase.GUIDToAssetPath(sortedItem));
+                results.Add(asset.name);
+            }
+
+            return results.Distinct().ToArray();
+        }
+
+
+        private static void ConfigGroup(HotUpdateLoaderConfig config, AddressableAssetSettings aa, string tempFolder)
+        {
+            string order = null;
+
+            foreach (var entry in config.AssemblyGroup.entries)
+            {
+                if (entry.address == $"AssemblyOrder.{EditorUserBuildSettings.activeBuildTarget}")
+                {
+                    order = entry.guid;
+                }
+            }
+
+            orderAsset = AssetDatabase.LoadAssetAtPath<AssemblyOrder>($"{tempFolder}/AssemblyOrder.{EditorUserBuildSettings.activeBuildTarget}.asset");
+
+            if (order == null)
+            {
+                if (orderAsset == null)
+                {
+                    orderAsset = CreateInstance<AssemblyOrder>();
+                    AssetDatabase.CreateAsset(orderAsset, $"{tempFolder}/AssemblyOrder.{EditorUserBuildSettings.activeBuildTarget}.asset");
+
+                }
+                order = AssetDatabase.AssetPathToGUID($"{tempFolder}/AssemblyOrder.{EditorUserBuildSettings.activeBuildTarget}.asset");
+            }
+
+            aa.CreateOrMoveEntry(order, config.AssemblyGroup, true)
+            .SetAddress($"AssemblyOrder.{EditorUserBuildSettings.activeBuildTarget}");
+
+            EditorUtility.SetDirty(config.AssemblyGroup);
+        }
+
     }
 }
 #endif
